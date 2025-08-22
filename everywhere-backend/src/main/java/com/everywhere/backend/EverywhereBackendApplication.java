5ca1d994bf63870943cc658880d97c538825aaa5
eddyacv <dylan.acevedo2843@gmail.com>
2025-08-22 14:39:55 -0500

    feat:Estructura

---
+++ everywhere-backend/src/main/java/com/everywhere/backend/EverywhereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.everywhere.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverywhereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverywhereBackendApplication.class, args);
+    }
+
+}
