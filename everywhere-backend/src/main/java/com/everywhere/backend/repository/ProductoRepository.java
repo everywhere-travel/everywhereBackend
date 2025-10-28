d809adcbc658cf8b5e683c7f70ea7c116b8afadd
jaime <jpalominoc5@upao.edu.pe>
2025-10-27 19:13:21 -0500

    feat: .

--- everywhere-backend/src/main/java/com/everywhere/backend/repository/ProductoRepository.java
+++ everywhere-backend/src/main/java/com/everywhere/backend/repository/ProductoRepository.java
@@ -3,7 +3,5 @@ package com.everywhere.backend.repository;
 import com.everywhere.backend.model.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 }
